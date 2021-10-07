f2e2865cc93f017490b1a6f641816cd99cd5a7e3
Xakimov1610 <nosirbekxakimov01@gmail.com>
2021-10-07 21:17:03 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bootcamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
